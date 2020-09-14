90a0dc9cb4905a9559e848704471eeb0eb2e4dc3
slavic-sky <44680193+slavic-sky@users.noreply.github.com>
2020-09-14 18:24:11 +0300

    refactoring from a scratch

--- app/src/main/java/ru/moviedbapp/slavicsky/service/ApiFactory.kt
+++
@@ -1,8 +0,0 @@
-package ru.moviedbapp.slavicsky.service
-
-import ru.moviedbapp.slavicsky.AppConstants
-
-object ApiFactory{
-    val tmdbApi : TmdbApi = RetrofitFactory.retrofit(AppConstants.TMDB_BASE_URL)
-        .create(TmdbApi::class.java)
-}
