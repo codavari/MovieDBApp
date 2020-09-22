033ef153ef9ad6681f63db241a2f04ecca5c2f71
slavic-sky <44680193+slavic-sky@users.noreply.github.com>
2020-09-22 12:19:58 +0300

    + NetworkModule.kt, separate gradle file for versions, HttpRequestInterceptor.kt

---
+++ app/src/main/java/ru/moviedbapp/codavari/di/NetworkModule.kt
@@ -0,0 +1,42 @@
+package ru.moviedbapp.codavari.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import ru.moviedbapp.codavari.api.MovieApiService
+import ru.moviedbapp.codavari.models.network.HttpRequestInterceptor
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpRequestInterceptor())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit{
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoviesService(retrofit: Retrofit): MovieApiService{
+        return retrofit.create(MovieApiService::class.java)
+    }
+}
