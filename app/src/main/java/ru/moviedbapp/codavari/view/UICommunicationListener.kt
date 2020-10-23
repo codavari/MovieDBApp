6cd400ef2ce50518fa6e11d918e43d1c70297be0
slavic-sky <44680193+slavic-sky@users.noreply.github.com>
2020-10-23 21:46:59 +0300

    some more code

---
+++ app/src/main/java/ru/moviedbapp/codavari/view/UICommunicationListener.kt
@@ -0,0 +1,18 @@
+package ru.moviedbapp.codavari.view
+
+import ru.moviedbapp.codavari.util.Response
+import ru.moviedbapp.codavari.util.StateMessageCallback
+
+interface UICommunicationListener {
+
+    fun onResponseReceived(
+        response: Response,
+        stateMessageCallback: StateMessageCallback
+    )
+
+    fun displayProgressBar(isLoading: Boolean)
+
+    fun expandAppBar()
+
+
+}
--- app/src/main/java/ru/moviedbapp/codavari/view/movies/MoviesFragment.kt
+++ app/src/main/java/ru/moviedbapp/codavari/view/movies/MoviesFragment.kt
@@ -1,25 +1,31 @@
 package ru.moviedbapp.codavari.view.movies
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.RequestManager
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_movie_list.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import ru.moviedbapp.R
-import ru.moviedbapp.codavari.models.entity.Movie
+import ru.moviedbapp.codavari.states.MovieStateEvent
+import ru.moviedbapp.codavari.util.Response
+import ru.moviedbapp.codavari.util.StateMessageCallback
+import ru.moviedbapp.codavari.view.UICommunicationListener
+import timber.log.Timber
 
-class MoviesFragment : Fragment() {
+@AndroidEntryPoint
+@FlowPreview
+@ExperimentalCoroutinesApi
+class MoviesFragment : Fragment(), UICommunicationListener {
 
     private val moviesViewModel: MoviesViewModel by activityViewModels()
     private lateinit var adapter: MovieAdapter
-    private var requestManager: RequestManager? = null
-
+    lateinit var uiCommunicationListener: UICommunicationListener
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +33,8 @@ class MoviesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         initUI()
+        moviesViewModel.setStateEvent(MovieStateEvent.NowPlayingEvent)
+        subscribeUI()
         return inflater.inflate(R.layout.fragment_movie_list, container)
     }
 
@@ -35,18 +43,35 @@ class MoviesFragment : Fragment() {
         rv_movies_list.adapter = MovieAdapter()
     }
 
+    override fun displayProgressBar(isLoading: Boolean) {
+
+    }
+
     private fun subscribeUI() {
+        moviesViewModel.numActiveJobs.observe(viewLifecycleOwner, {
+            uiCommunicationListener.displayProgressBar(moviesViewModel.areAnyJobsActive())
+        })
 
         moviesViewModel.viewState.observe(viewLifecycleOwner, { viewState ->
             if (viewState != null) {
                 adapter.apply {
                     viewState.movies?.let {
-
                         differ.submitList(it)
-                        Log.d(TAG, "subscribeObservers: differ")
+                        Timber.d("subscribeObservers: differ")
                     }
                 }
             }
         })
+    }
+
+    override fun onResponseReceived(
+        response: Response,
+        stateMessageCallback: StateMessageCallback
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun expandAppBar() {
+        TODO("Not yet implemented")
     }
 }
